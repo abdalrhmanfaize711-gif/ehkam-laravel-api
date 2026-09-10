fad744da62282b6c5febbe7a743907ca4422a634
abdalrhmanfaize711-gif <abdalrhmanfaize711@gmail.com>
2026-09-10 14:37:12 +0300

    update Seeder adding 2

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -26,5 +26,9 @@ class DatabaseSeeder extends Seeder
                 $this->call([
             AdminSeeder::class,
         ]);
+            $this->call([
+            RegionsSeeder::class,
+        ]);
+        
     }
 }
